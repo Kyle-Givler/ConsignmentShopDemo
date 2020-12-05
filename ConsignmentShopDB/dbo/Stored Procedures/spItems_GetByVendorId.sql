8965f1b374efce5c8d7d9d26677d0a32bf260456
Kyle Givler <xvegan88x@gmail.com>
2020-12-05 18:40:15 -0500

    Add stored proc

---
+++ ConsignmentShopDB/dbo/Stored Procedures/spItems_GetByVendorId.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spItems_GetByVendorId]
+	@OwnerId int
+AS
+begin
+
+	set nocount on;
+
+	select [Id], [Name], [Description], [Price], [Sold], [OwnerId], [PaymentDistributed] 
+	from dbo.Items
+	where OwnerId = @OwnerId;
+
+end
